245ff8a07c5fa766d4ac5d80fb183fdc57226ab1
= <jwparker284@gmail.com>
2023-08-27 09:51:23 -0700

    completed linking data, and added a bit more data rows

--- data.js
+++ data.js
@@ -6,7 +6,9 @@ const pets = [
         breed: 'Labrador',
         age: 3,
         owner: 'John',
+        ownerurl: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/1200px-SpongeBob_SquarePants_character.svg.png",
         telephone: '555-555-5555',
+        peturl:"https://i0.wp.com/theverybesttop10.com/wp-content/uploads/2018/04/Ten-of-the-Funniest-Labrador-Retrievers-You-Will-Ever-See-10.jpg?resize=600%2C464&ssl=1",
         appointments: [
 
             {
@@ -23,6 +25,8 @@ const pets = [
         age: 2,
         owner: 'Jane',
         telephone: '555-555-5555',
+        ownerurl:"https://w7.pngwing.com/pngs/503/1/png-transparent-patrick-star-patrick-star-saint-patrick-s-day-paddy-holidays-fictional-character-saint-patricks-day.png",
+        peturl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpoV2NCuEQI_irPoOabUYTuCLQ6Sv3HHA8tbdW3qUoNBGqFTf9EB2in2Oil-Kj4pNMDqY&usqp=CAU",
         appointments: [
             {
                 date: '01/01/2020',
@@ -38,7 +42,9 @@ const pets = [
         breed: 'Pitbull',
         age: 1,
         owner: 'John',
+        ownerurl:"https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Squidward_Tentacles.svg/1200px-Squidward_Tentacles.svg.png",
         telephone: '555-555-5555',
+        peturl:"https://i.ytimg.com/vi/AQ7pxuXhL64/maxresdefault.jpg",
         appointments: [
             {
                 date: '01/01/2020',
--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // import the pets array from data.js
 const pets = require('./data');
+const path = require("path");
+const cors = require("cors")
 
 // init express app
 const express = require('express');
+
 const app = express();
+app.use(cors());
 
 const PORT = 8080;
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
+    //do this with res.sendFile. We can send the whole html file.
+    //res.sendFile("/home/jerrysunix/reactStuff/Coursework/block31/petFinder/Unit4.PetFinder.Starter/public/index.html")
+    res.sendFile("index.html",{ root: path.join(__dirname, "./public")})
+
 
 });
 
@@ -21,30 +29,55 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
+    //send back the information from the pets database through res
+    res.send(pets)
+
 
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
+    //do this by doing an express query on the req, then sending back the pets in with the res
+    const owner = req.query.owner;
+    console.log(owner);
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
+    console.log(`pet: ${pet}`)
 
     // send the pet as a response
+    //send back a response as pet
+    //res.send(pet);
+    if (pet !== undefined) {
+        res.send(pet);
+    } else {
+        res.send("No such owner exists!");
+    }
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
+    //do this by getting the query info from the request
+    const name= req.params.name;
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
+
     // send the pet as a response
+    //make sure it's not null, and send back a pet object
+    console.log(pet);
+    if (pet!== undefined) {
+        res.send(pet);
+    } else {
+        res.send("no such pet name exists here!");
+    }
+    
 
 });
 
